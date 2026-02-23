356d6e139122af123f60eac1779cbaf1e1cd0a1e
Sibghat776 <ullahsibghat786@gmail.com>
2026-02-23 22:21:46 +0500

    vercel.json file in backend

--- Backend/index.js
+++ Backend/index.js
@@ -20,18 +20,17 @@ app.use(cors({
 app.use(helmet())
 
 if (process.env.NODE_ENV !== "production") {
-    (async () => {
-        try {
-            await connectDB();
-            app.listen(process.env.PORT || 5000, () => {
-                console.log(`🚀 Server running on ${process.env.PORT || 5000}`);
-            });
-        } catch (err) {
-            console.error("DB connection failed:", err);
-            process.exit(1);
-        }
-    })();
+  const PORT = process.env.PORT || 5000;
+  connectDB().then(() => {
+    app.listen(PORT, () => console.log(`🚀 Server running on ${PORT}`));
+  }).catch(err => {
+    console.error("DB connection failed:", err);
+    process.exit(1);
+  });
 }
+
+export default app;
+
 app.use("/api/contact", contactRoute)
 app.use("/api/gallery", galleryRouter)
 app.use("/api/registration", registrationRouter)
